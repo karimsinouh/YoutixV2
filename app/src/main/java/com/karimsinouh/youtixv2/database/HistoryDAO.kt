60d1d67b9657dd1941671068e3fa4637d12395e3
karim <sinouhkar@gmail.com>
2021-03-16 14:01:49 -0700

    Setup DAOs

--- app/src/main/java/com/karimsinouh/youtixv2/database/HistoryDAO.kt
+++ app/src/main/java/com/karimsinouh/youtixv2/database/HistoryDAO.kt
@@ -1,9 +1,32 @@
 package com.karimsinouh.youtixv2.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.karimsinouh.youtixv2.data.entities.HistoryItem
 
 
 @Dao
 interface HistoryDAO {
+
+    @Query("SELECT 1 FROM HistoryItem")
+    fun list():LiveData<List<HistoryItem>>
+
+    @Query("SELECT * FROM HistoryItem WHERE id =:videoId")
+    suspend fun get(videoId: String):HistoryItem
+
+    @Insert(entity = HistoryItem::class)
+    suspend fun add(item:HistoryItem)
+
+    @Delete(entity = HistoryItem::class)
+    suspend fun delete(item:HistoryItem)
+
+    @Query(" SELECT EXISTS (SELECT * FROM HistoryItem WHERE videoId=:videoId) ")
+    suspend fun exists(videoId:String):Boolean
+
+    @Query("UPDATE HistoryItem SET currentMillis =:millis WHERE videoId=:videoId ")
+    suspend fun updateMillis(videoId:String,millis:Long)
 
 }
--- app/src/main/java/com/karimsinouh/youtixv2/database/WatchLaterDAO.kt
+++ app/src/main/java/com/karimsinouh/youtixv2/database/WatchLaterDAO.kt
@@ -1,8 +1,26 @@
 package com.karimsinouh.youtixv2.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.karimsinouh.youtixv2.data.entities.WatchLater
 
 
 @Dao
 interface WatchLaterDAO {
+
+    @Query("SELECT * FROM WatchLater")
+    fun list(item:WatchLater):LiveData<List<WatchLater>>
+
+    @Delete(entity = WatchLater::class)
+    suspend fun delete(item:WatchLater)
+
+    @Insert(entity = WatchLater::class)
+    suspend fun add(item:WatchLater)
+
+    @Query("SELECT EXISTS (SELECT 1 FROM WatchLater WHERE videoId=:videoId) ")
+    suspend fun exists(videoId:String)
+
 }
