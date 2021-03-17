2368627dcadd22765c064479126b55dbad98879f
karim <sinouhkar@gmail.com>
2021-03-17 03:28:34 -0700

    setup add to history in video player screen

--- app/src/main/java/com/karimsinouh/youtixv2/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/karimsinouh/youtixv2/ui/menu/MenuFragment.kt
@@ -1,7 +1,33 @@
 package com.karimsinouh.youtixv2.ui.menu
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.karimsinouh.youtixv2.R
+import com.karimsinouh.youtixv2.databinding.FragmentMenuBinding
 
 class MenuFragment:Fragment(R.layout.fragment_menu) {
+
+    private lateinit var binding:FragmentMenuBinding
+    private lateinit var nav:NavController
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding= FragmentMenuBinding.bind(view)
+        nav=findNavController()
+
+
+        binding.history.setOnClickListener {
+
+        }
+
+        binding.watchLater.setOnClickListener {
+
+        }
+
+
+    }
+
 }
