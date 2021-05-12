b3db8a1d5051c674fad649833dec4c41979d9dbd
karim <sinouhkar@gmail.com>
2021-05-12 11:11:50 -0700

    setup workspace

--- app/src/main/java/com/karimsinouh/youtixv2/utils/Constants.kt
+++ app/src/main/java/com/karimsinouh/youtixv2/utils/Constants.kt
@@ -1,9 +1,12 @@
 package com.karimsinouh.youtixv2.utils
 
 const val BASE_URL="https://www.googleapis.com/youtube/v3/"
-const val CHANNEL_ID="UC_Fh8kvtkVPkeihBs42jGcA"
+
+const val CHANNEL_ID="UCR-vujr17AbIWWp3lL8sdpg"
+
 const val API_KEY="AIzaSyDr519sy5ktI22CMisfGcyYOjDfYhCXQP0"
-const val PLAYLIST_ID="UUX4mqbvv5lGqLpI4FYlJt4w"
+
+const val PLAYLIST_ID="UUR-vujr17AbIWWp3lL8sdpg"
 
 
 const val FEEDBACK_URL="https://google.com"
@@ -11,15 +14,10 @@ const val FEEDBACK_URL="https://google.com"
 
 
 const val VIDEO_ID="video_id"
-
 const val PLAYLIST_NAME="playlist_name"
-
 const val KIND_VIDEO="youtube#video"
-
 const val ACTION="action"
 const val ACTION_HISTORY="action_history"
 const val ACTION_WATCH_LATER="action_watch_later"
-
 const val SNIPPET="snippet"
-
 const val NOTIFICATION_CHANNEL_ID="notification01"
