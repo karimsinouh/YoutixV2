91ec958bc238ecd8b06d5b920119e2b1ee9be17f
karim <sinouhkar@gmail.com>
2021-05-15 01:13:18 -0700

    change get started screen a bit

--- app/src/main/java/com/karimsinouh/youtixv2/ui/getStarted/SquareImage.kt
+++ app/src/main/java/com/karimsinouh/youtixv2/ui/getStarted/SquareImage.kt
@@ -8,7 +8,8 @@ class SquareImage(context:Context,attributeSet: AttributeSet):
     AppCompatImageView(context,attributeSet) {
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+        val sixtyPercent=widthMeasureSpec/100*60
+        super.onMeasure(widthMeasureSpec, sixtyPercent)
     }
 
 }
